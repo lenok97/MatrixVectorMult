c4a3b6640f8d481934dc14d89970cd2a4592a0d6
lenok97 <iamhappluck@gmail.com>
2020-01-05 21:08:45 +0500

    ленточное разбиение

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -4,8 +4,8 @@
 #include <time.h>
 #include <mpi.h>
 #include <iostream>
-
 using namespace std;
+
 const int root = 0;
 
 void printMatrix(int n, int m, int* matrix)
@@ -13,9 +13,7 @@ void printMatrix(int n, int m, int* matrix)
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++)
-		{
 			cout << matrix[n * i + j] << '\t';
-		}
 		cout << endl;
 	}
 	cout << endl;
@@ -25,9 +23,7 @@ void generateSimpleMatrix(int n, int m, int* matrix)
 {
 	for (int i = 0; i < m; i++)
 		for (int j = 0; j < n; j++)
-		{
 			matrix[n * i + j]=i;
-		}
 }
 
 void generateSimpleVector(int n, int* vector)
@@ -40,32 +36,28 @@ void generateRandomMatrix(int n, int m, int* matrix)
 {
 	for (int i = 0; i < m; i++)
 		for (int j = 0; j < n; j++)
-		{
 			matrix[n * i + j] =  rand() % 100;
-		}
 }
 
 int main(int argc, char *argv[])
 {
-
 	int rank, size;
 	double endTime, startTime;
-
+	bool randomMatrix = false;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
 	while (true)
 	{
+		int n, workPerProc, extraWork, rowsCount;
 		int* matrix = NULL;
 		int* vector = NULL;
 		int* result = NULL;
-		int n, workPerProc, extraWork, myRowsSize;
-
-		int* sendcounts = new int[size];
-		int* senddispls = new int[size];
-		int* recvcounts = new int[size];
-		int* recvdispls = new int[size];
+		// MPI_Scatterv/MPI_Gatherv params
+		int* sendcounts = new int[size]; //количество элементов, принимаемых от каждого процесса
+		int* senddispls = new int[size]; //начало расположения элементов блока, посылаемого i-му процессу
+		int* recvcounts = new int[size]; //количество элементов, отправляемых каждым процессом
+		int* recvdispls = new int[size]; //начало расположения элементов блока, принимаемых от i-ого процесса
 
 		if (rank == root)
 		{
@@ -74,88 +66,93 @@ int main(int argc, char *argv[])
 			cin >> n;
 
 			matrix = new int[n*n];
-
-			cout << "matrix:" << endl;
-			generateSimpleMatrix(n, n, matrix);
-			//generateRandomMatrix(N, M, matrix);
-			if (n<100)
-				printMatrix(n, n, matrix);
-
 			vector = new int[n];
-			generateSimpleVector(n, vector);
-			//generateRandomMatrix(1, N, vector);
-			cout << "vector:" << endl;
+
+			if (randomMatrix)
+			{
+				generateRandomMatrix(n, n, matrix);
+				generateRandomMatrix(1, n, vector);
+			}
+			else
+			{
+				generateSimpleMatrix(n, n, matrix);
+				generateSimpleVector(n, vector);
+			}
 			if (n < 100)
-			printMatrix(1, n, vector);
+			{
+				cout << "matrix:" << endl;
+				printMatrix(n, n, matrix);
+				cout << "vector:" << endl;
+				printMatrix(1, n, vector);
+			}
 			startTime = MPI_Wtime();
 
 			workPerProc = n / size;
-			extraWork = n - workPerProc * size;
+			extraWork = n % size;;
 			int prefixSum = 0;
-			for (int i = 0; i < size; ++i)
+			// MPI_Scatterv/MPI_Gatherv params
+			for (int i = 0; i < size; i++)
 			{
-				recvcounts[i] = (i < extraWork) ? workPerProc + 1 : workPerProc;
+				recvcounts[i] = workPerProc;
+				if (i < extraWork)
+					recvcounts[i]++; 
 				sendcounts[i] = recvcounts[i] * n;
 				recvdispls[i] = prefixSum;
 				senddispls[i] = prefixSum * n;
 				prefixSum += recvcounts[i];
 			}
+			result = new int[n];
 		}
 
 		MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
 		if (rank != root)
 			vector = new int[n];
-		MPI_Bcast(vector,n, MPI_INT, 0, MPI_COMM_WORLD);
-
+		MPI_Bcast(vector, n, MPI_INT, 0, MPI_COMM_WORLD);
+		// горизонтаальное ленточное разбиение
 		workPerProc = n / size;
 		extraWork = n % size;
-		myRowsSize = rank < extraWork ? workPerProc + 1 : workPerProc;
-		int* matrixPart = new int[myRowsSize * n];
-
+		rowsCount = workPerProc;
+		if (rank < extraWork)
+			rowsCount++;
+		int* matrixPart = new int[rowsCount * n];
+		
 		// разбивает сообщение из буфера посылки процесса root на части
-		//Начало расположения элементов блока, посылаемого i - му процессу, 
-		//задается в массиве смещений displs, а число посылаемых элементов - в массиве sendcounts
-		MPI_Scatterv(matrix, sendcounts, senddispls, MPI_INT, matrixPart, myRowsSize * n, MPI_INT, root, MPI_COMM_WORLD);
+		MPI_Scatterv(matrix, sendcounts, senddispls, MPI_INT, matrixPart, rowsCount * n, MPI_INT, root, MPI_COMM_WORLD);
 		if (rank == root)
 		{
 			delete[] sendcounts;
 			delete[] senddispls;
 			delete[] matrix;
 		}
-
-		int* resultPart = new int[myRowsSize];
-
-		for (int i = 0; i < myRowsSize; ++i)
+		// printMatrix(n, rowsCount, matrixPart);
+		int* tempResult = new int[rowsCount];
+		for (int i = 0; i < rowsCount; ++i)
 		{
-			resultPart[i] = 0;
-			for (int j = 0; j < n; ++j)
+			tempResult[i] = 0;
+			for (int j = 0; j < n; j++)
 			{
-				resultPart[i] += matrixPart[i *n + j] * vector[j];
+				tempResult[i] += matrixPart[i *n + j] * vector[j];
 			}
 		}
+		// printMatrix(rowsCount, 1, tempResult);
+
+		// собирает блоки с разным числом элементов от каждого процесса
+		MPI_Gatherv(tempResult, rowsCount, MPI_INT, result, recvcounts, recvdispls, MPI_INT, root, MPI_COMM_WORLD);
 		delete[] matrixPart;
 		delete[] vector;
+		delete[] tempResult;
 
-		if (rank == root)
-			result = new int[n];
-		//собирает блоки с разным числом элементов от каждого процесса, 
-		//количество элементов, принимаемых от каждого процесса, задается в массиве recvcounts.
-		//Эта функция обеспечивает также большую гибкость при размещении данных в процессе - получателе, 
-		//благодаря введению в качестве параметра массива смещений displs.
-		MPI_Gatherv(resultPart, myRowsSize, MPI_INT, result, recvcounts, recvdispls, MPI_INT, root, MPI_COMM_WORLD);
-		delete[] resultPart;
 		if (rank == root)
 		{
 			endTime = MPI_Wtime();
-			delete[] recvcounts;
-			delete[] recvdispls;
 			cout << "result:" << endl;
 			printMatrix(1, n, result);
+			delete[] recvcounts;
+			delete[] recvdispls;
 			delete[] result;
 			cout << endl << "Time elapsed: " << (endTime - startTime) * 1000 << "ms\n" << endl;
+			randomMatrix = !randomMatrix;
 		}
-
 	}
 	MPI_Finalize();
 	return 0;
