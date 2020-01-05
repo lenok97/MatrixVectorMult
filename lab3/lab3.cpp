9630932f2aaf48c9e095c40e91e3a17743c52d4c
lenok97 <iamhappluck@gmail.com>
2020-01-05 20:16:45 +0500

    M=N=n

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -48,76 +48,75 @@ void generateRandomMatrix(int n, int m, int* matrix)
 int main(int argc, char *argv[])
 {
 
-		int rank, size, M, N, workPerProc, extraWork, myRowsSize;
-		double endTime, startTime;
+	int rank, size;
+	double endTime, startTime;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	while (true)
+	{
 		int* matrix = NULL;
 		int* vector = NULL;
 		int* result = NULL;
-
-		MPI_Init(&argc, &argv);
-		MPI_Comm_size(MPI_COMM_WORLD, &size);
-		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+		int n, workPerProc, extraWork, myRowsSize;
 
 		int* sendcounts = new int[size];
 		int* senddispls = new int[size];
 		int* recvcounts = new int[size];
 		int* recvdispls = new int[size];
-		while (true)
-		{
+
 		if (rank == root)
 		{
 			cout << "Matrix multiplication by vector" << endl << "Process count = " << size << endl;
-			cout << "Enter the number of matrix rows:" << endl;
-			cin >> M;
-			cout << "Enter the number of matrix columns:" << endl << endl;
-			cin >> N;
+			cout << "Enter n:" << endl;
+			cin >> n;
 
-			matrix = new int[M * N];
+			matrix = new int[n*n];
 
 			cout << "matrix:" << endl;
-			generateSimpleMatrix(N, M, matrix);
+			generateSimpleMatrix(n, n, matrix);
 			//generateRandomMatrix(N, M, matrix);
-			if (N<100)
-				printMatrix(N, M, matrix);
+			if (n<100)
+				printMatrix(n, n, matrix);
 
-			vector = new int[N];
-			generateSimpleVector(N, vector);
+			vector = new int[n];
+			generateSimpleVector(n, vector);
 			//generateRandomMatrix(1, N, vector);
 			cout << "vector:" << endl;
-			if (N < 100)
-			printMatrix(1, N, vector);
+			if (n < 100)
+			printMatrix(1, n, vector);
 			startTime = MPI_Wtime();
 
-			workPerProc = M / size;
-			extraWork = M - workPerProc * size;
+			workPerProc = n / size;
+			extraWork = n - workPerProc * size;
 			int prefixSum = 0;
 			for (int i = 0; i < size; ++i)
 			{
 				recvcounts[i] = (i < extraWork) ? workPerProc + 1 : workPerProc;
-				sendcounts[i] = recvcounts[i] * N;
+				sendcounts[i] = recvcounts[i] * n;
 				recvdispls[i] = prefixSum;
-				senddispls[i] = prefixSum * N;
+				senddispls[i] = prefixSum * n;
 				prefixSum += recvcounts[i];
 			}
 		}
 
-		MPI_Bcast(&M, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+		MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 		if (rank != root)
-			vector = new int[N];
-		MPI_Bcast(vector, N, MPI_INT, 0, MPI_COMM_WORLD);
+			vector = new int[n];
+		MPI_Bcast(vector,n, MPI_INT, 0, MPI_COMM_WORLD);
 
-
-		workPerProc = M / size;
-		extraWork = M % size;
+		workPerProc = n / size;
+		extraWork = n % size;
 		myRowsSize = rank < extraWork ? workPerProc + 1 : workPerProc;
-		int* matrixPart = new int[myRowsSize * N];
+		int* matrixPart = new int[myRowsSize * n];
 
 		// разбивает сообщение из буфера посылки процесса root на части
 		//Начало расположения элементов блока, посылаемого i - му процессу, 
 		//задается в массиве смещений displs, а число посылаемых элементов - в массиве sendcounts
-		MPI_Scatterv(matrix, sendcounts, senddispls, MPI_INT, matrixPart, myRowsSize * N, MPI_INT, root, MPI_COMM_WORLD);
+		MPI_Scatterv(matrix, sendcounts, senddispls, MPI_INT, matrixPart, myRowsSize * n, MPI_INT, root, MPI_COMM_WORLD);
 		if (rank == root)
 		{
 			delete[] sendcounts;
@@ -130,16 +129,16 @@ int main(int argc, char *argv[])
 		for (int i = 0; i < myRowsSize; ++i)
 		{
 			resultPart[i] = 0;
-			for (int j = 0; j < N; ++j)
+			for (int j = 0; j < n; ++j)
 			{
-				resultPart[i] += matrixPart[i * N + j] * vector[j];
+				resultPart[i] += matrixPart[i *n + j] * vector[j];
 			}
 		}
 		delete[] matrixPart;
 		delete[] vector;
 
 		if (rank == root)
-			result = new int[M];
+			result = new int[n];
 		//собирает блоки с разным числом элементов от каждого процесса, 
 		//количество элементов, принимаемых от каждого процесса, задается в массиве recvcounts.
 		//Эта функция обеспечивает также большую гибкость при размещении данных в процессе - получателе, 
@@ -152,7 +151,7 @@ int main(int argc, char *argv[])
 			delete[] recvcounts;
 			delete[] recvdispls;
 			cout << "result:" << endl;
-			printMatrix(1, N, result);
+			printMatrix(1, n, result);
 			delete[] result;
 			cout << endl << "Time elapsed: " << (endTime - startTime) * 1000 << "ms\n" << endl;
 		}
